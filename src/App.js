4698ebfdbebde7f2c9ee1b6b8e00fb3175814894
Anika Mlodzianowski <anikamlodzianowski@MotMot.attlocal.net>
2021-03-06 19:10:34 -0800

    fixed codebuild permissions

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          WELCOME TO F1 App!!!!!!!!!!!!!!!
         </a>
       </header>
     </div>
