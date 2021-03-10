278d877892c91e7661e79d89eb48138c1ede59f6
Anika Mlodzianowski <anikamlodzianowski@gmail.com>
2021-03-09 21:22:01 -0800

    fixed bugs

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-   <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
