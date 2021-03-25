37274d27f084dd2a5cd697e9b2eaf7095f14913e
Anika Mlodzianowski <anikamlodzianowski@gmail.com>
2021-03-24 19:49:11 -0700

    styling all components

--- src/components/DatePickers.tsx
+++ src/components/DatePickers.tsx
@@ -1,9 +1,10 @@
 import DateFnsUtils from '@date-io/date-fns';
 import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
-import { createMuiTheme } from "@material-ui/core";
+import { createMuiTheme, useMediaQuery } from "@material-ui/core";
 import { ThemeProvider, makeStyles } from "@material-ui/styles";
 
 import {RaceDate} from '../libs/interfaces';
+import theme from "../utils/theme";
 
 const datePickerTheme = createMuiTheme({
   palette: {
@@ -14,8 +15,23 @@ const datePickerTheme = createMuiTheme({
 });
 
 const useStyles = makeStyles({
-  datePicker: {
-    margin: '2em',
+  datePickerWeb: {
+    margin: '2em 2em 4em',
+    '& .MuiInputBase-input': {
+      '& .MuiInput-input': {
+        fontFamily: "Russo One",
+      },
+      fontFamily: "Russo One",
+    },
+  },
+  inputBase: {
+    fontFamily: "Russo One",
+  },
+  datePickerMobile: {
+    margin: '0em 2em 2em',
+  },
+  inputLabel: {
+    fontFamily: "Russo One",
   }
 });
 
@@ -28,6 +44,7 @@ interface DatePickersPropsType {
 
 export default function DatePickers(props:DatePickersPropsType): JSX.Element {
   const classes = useStyles();
+  const breakpointSM = useMediaQuery(theme.breakpoints.up('sm'));
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <ThemeProvider theme={datePickerTheme}>
@@ -40,7 +57,13 @@ export default function DatePickers(props:DatePickersPropsType): JSX.Element {
           label="Start Date"
           value={props.startDate}
           onChange={(newStartDate: RaceDate) => props.setStartDateChange(newStartDate)}
-          className={classes.datePicker}
+          className={breakpointSM? classes.datePickerWeb : classes.datePickerMobile}
+          InputProps={{
+            className: classes.inputBase 
+          }}
+          InputLabelProps={{
+            className: classes.inputLabel 
+          }}
         />
         <DatePicker
           autoOk
@@ -51,7 +74,13 @@ export default function DatePickers(props:DatePickersPropsType): JSX.Element {
           label="End Date"
           value={props.endDate}
           onChange={(newEndDate: RaceDate) => props.setEndDateChange(newEndDate)}
-          className={classes.datePicker}
+          className={breakpointSM? classes.datePickerWeb : classes.datePickerMobile}
+          InputProps={{
+            className: classes.inputBase 
+          }}
+          InputLabelProps={{
+            className: classes.inputLabel 
+          }}
         />
       </ThemeProvider>
     </MuiPickersUtilsProvider>
--- src/components/charts/ChartSelector.tsx
+++ src/components/charts/ChartSelector.tsx
@@ -1,13 +1,41 @@
 import { useState } from "react";
 import * as d3 from 'd3';
-import { Button, Typography } from '@material-ui/core/';
+import { Button, Container, Typography } from '@material-ui/core/';
+import { makeStyles } from "@material-ui/styles";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import { GraphSelectorPropsType, CombinedRaceCircuitPropertyEnum } from '../../libs/interfaces';
 import {getPropertyArray} from '../../utils/helpers'
 import BarChart from './BarChart';
+import theme from "../../utils/theme";
+
+const useStyles = makeStyles({
+  button: {
+    borderRadius: 15,
+    fontFamily: "Russo One",
+    width: 'auto'
+  },
+  buttonMobile: {
+    fontSize: 'small'
+  },
+  container: {
+    display: 'flex',
+    padding: "0 0.5em",
+    margin: "1em 0",
+    justifyContent: "center",
+  },
+  typography: {
+    margin: '1em 0',
+    fontFamily: "Russo One",
+    letterSpacing: 10,
+    fontWeight: 700
+  }
+});
 
 export default function ChartSelector({raceData}: GraphSelectorPropsType) {
+  const classes = useStyles();
   const [selectedButton, setSelectedButton] = useState(CombinedRaceCircuitPropertyEnum.circuitName)
+  const breakpointSM = useMediaQuery(theme.breakpoints.up('sm'));
 
   let allData = {
     [CombinedRaceCircuitPropertyEnum.circuitName]: getPropertyArray(raceData, CombinedRaceCircuitPropertyEnum.circuitName).sort((a, b) => d3.descending(a.value, b.value)),
@@ -17,28 +45,37 @@ export default function ChartSelector({raceData}: GraphSelectorPropsType) {
 
   return (
     <>
-    <Typography>{selectedButton}</Typography>
-      <Button 
-        variant='contained'
-        onClick={() => setSelectedButton(CombinedRaceCircuitPropertyEnum.circuitName)}
-        color={selectedButton === CombinedRaceCircuitPropertyEnum.circuitName? 'primary' : 'secondary'}
-      >
-        Circuits
-      </Button>
-      <Button 
-        variant='contained'
-        onClick={() => setSelectedButton(CombinedRaceCircuitPropertyEnum.driver_win)}
-        color={selectedButton === CombinedRaceCircuitPropertyEnum.driver_win? 'primary' : 'secondary'}
-      >
-        Drivers
-      </Button>
-      <Button 
-        variant='contained'
-        onClick={() => setSelectedButton(CombinedRaceCircuitPropertyEnum.constructor_win)}
-        color={selectedButton === CombinedRaceCircuitPropertyEnum.constructor_win? 'primary' : 'secondary'}
-      >
-        Constructors
-      </Button>
+      <Typography id='statistics' variant='h6' className={classes.typography}>Statistics</Typography>
+      <Container maxWidth="lg" className={classes.container}>
+        <Button 
+          variant='contained'
+          onClick={() => setSelectedButton(CombinedRaceCircuitPropertyEnum.circuitName)}
+          color={selectedButton === CombinedRaceCircuitPropertyEnum.circuitName? 'primary' : 'secondary'}
+          className={breakpointSM? classes.button : classes.buttonMobile + ' ' + classes.button}
+          size={breakpointSM? 'medium': 'small'}
+        >
+          Circuits
+        </Button>
+        <Button 
+          variant='contained'
+          onClick={() => setSelectedButton(CombinedRaceCircuitPropertyEnum.driver_win)}
+          color={selectedButton === CombinedRaceCircuitPropertyEnum.driver_win? 'primary' : 'secondary'}
+          className={breakpointSM? classes.button : classes.buttonMobile + ' ' + classes.button}
+          size={breakpointSM? 'medium': 'small'}
+          style={{margin: "0 0.5em"}}
+        >
+          Drivers
+        </Button>
+        <Button 
+          variant='contained'
+          onClick={() => setSelectedButton(CombinedRaceCircuitPropertyEnum.constructor_win)}
+          color={selectedButton === CombinedRaceCircuitPropertyEnum.constructor_win? 'primary' : 'secondary'}
+          className={breakpointSM? classes.button : classes.buttonMobile + ' ' + classes.button}
+          size={breakpointSM? 'medium': 'small'}
+        >
+          Constructors
+        </Button>
+      </Container>
       <BarChart
         key={selectedButton}
         data={allData[selectedButton]}
